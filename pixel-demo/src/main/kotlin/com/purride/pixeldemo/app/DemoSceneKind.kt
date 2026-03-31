c54496fa2bfda4ac860d91696d822800159ed5c6
xiangyu <xiangyu@szjiuzhou.com>
2026-03-31 20:04:22 +0800

    feat(demo): 增加表单与列表组合场景

--- pixel-demo/src/main/kotlin/com/purride/pixeldemo/app/DemoSceneKind.kt
+++ pixel-demo/src/main/kotlin/com/purride/pixeldemo/app/DemoSceneKind.kt
@@ -12,6 +12,7 @@ enum class DemoSceneKind(
     HORIZONTAL_PAGER("横向分页"),
     VERTICAL_PAGER("纵向分页"),
     LIST("纵向列表"),
+    FORM_AND_LIST("表单与列表组合"),
     PAGER_AND_LIST("分页与列表组合"),
     LAYOUT_AND_CLICK("布局与点击"),
 }
